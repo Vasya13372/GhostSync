499ae93056d0c4f44c2ebb7aabbdf22e07cd8ba1
Vasya13372 <149879396+Vasya13372@users.noreply.github.com>
2025-02-15 11:56:06 +0300

    Update setup.lua

--- setup.lua
+++ setup.lua
@@ -116,24 +116,6 @@ debug.getupvalue = function(v48, v49)
     return v50
 end
 
-----------
-debug.setupvalue = function(func, upvalueIndex, value)
-    -- Получаем текущее окружение функции
-    local currentEnv = getfenv(func)
-    
-    -- Мы создаем новую функцию, которая будет служить мостом
-    local newFunc = function(...)
-        -- Устанавливаем значение для upvalue
-        currentEnv[upvalueIndex] = value
-        -- Вызов оригинальной функции с переданными аргументами
-        return func(...)
-    end
-    
-    -- Устанавливаем новое окружение для новой функции
-    setfenv(newFunc, currentEnv)
-    return newFunc
-end
-----------
 
 local v0 = table
 table = v0.clone(v0)
